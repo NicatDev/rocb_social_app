4871656d3e68397ad2a2b98fc36d6752ff7d0e27
NicatDev <nicat254memmedov@gmail.com>
2025-10-04 15:33:00 +0400

    main

--- content/admin.py
+++ content/admin.py
@@ -1,23 +1,34 @@
 from django.contrib import admin
 from .models import Post, Tag, Review, Like
 
-@admin.action(description="Seçilmiş postları qəbul et (Accept selected posts)")
-def accept_posts(modeladmin, request, queryset):
-    queryset.update(is_active=True)
 
-@admin.action(description="Seçilmiş postları rədd et (Decline selected posts)")
-def decline_posts(modeladmin, request, queryset):
-    queryset.update(is_active=False)
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+    fields = ('user', 'content', 'created_date')
+    readonly_fields = ('created_date',)
+
+
+class LikeInline(admin.TabularInline):
+    model = Like
+    extra = 0
+    fields = ('user', 'created_date')
+    readonly_fields = ('created_date',)
+
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'content_summary', 'created_date', 'is_active')
-    search_fields = ('content', 'user__username')
-    actions = [accept_posts, decline_posts] 
+    list_display = ('id', 'user', 'content_summary', 'is_active', 'created_date')
+    list_filter = ('is_active', 'created_date', 'tags')
+    search_fields = ('content', 'user__username', 'tags__name')
+    date_hierarchy = 'created_date'
+    filter_horizontal = ('tags',)
+    inlines = [ReviewInline, LikeInline]
 
     def content_summary(self, obj):
-        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+        return obj.content[:50] + ('...' if len(obj.content) > 50 else '')
     content_summary.short_description = 'Content'
 
+
+# Tags separately
 admin.site.register(Tag)
-admin.site.register(Post, PostAdmin)
-admin.site.register(Review)
-admin.site.register(Like)
