0d73b88176d557562a57e3d03c4890385204cd2e
NicatDev <nicat254memmedov@gmail.com>
2025-10-04 17:18:18 +0400

    main

--- content/serializers.py
+++ content/serializers.py
@@ -65,6 +65,8 @@ class PostApproveSerializer(serializers.ModelSerializer):
 
 class TopPostSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
+    first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
     profile_picture = serializers.ImageField(source='user.profile.profile_picture', read_only=True)
     like_count = serializers.IntegerField(source='likes.count', read_only=True)
     review_count = serializers.IntegerField(source='reviews.count', read_only=True)
