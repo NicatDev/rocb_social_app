26ea2a71f9dd1f27378a23569ab7c110f5c2b295
NicatDev <nicat254memmedov@gmail.com>
2025-10-12 21:51:29 +0400

    main

--- content/serializers.py
+++ content/serializers.py
@@ -65,23 +65,53 @@ class PostApproveSerializer(serializers.ModelSerializer):
 
 class TopPostSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
+    user = serializers.CharField(source='user.username', read_only=True)
     first_name = serializers.CharField(source='user.first_name', read_only=True)
     last_name = serializers.CharField(source='user.last_name', read_only=True)
     profile_picture = serializers.ImageField(source='user.profile.profile_picture', read_only=True)
+    
+    # Like və review count
     like_count = serializers.IntegerField(source='likes.count', read_only=True)
     review_count = serializers.IntegerField(source='reviews.count', read_only=True)
 
+    # Orijinal PostSerializer-dən olan sahələr
+    reviews = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+    liked_by_user = serializers.SerializerMethodField()
+    
     class Meta:
         model = Post
         fields = [
             'id',
             'content',
             'username',
-            'profile_picture',
-            'like_count',
-            'review_count',
-            'created_date',
+            'user',
             'first_name',
             'last_name',
-            'image'
+            'profile_picture',
+            'image',
+            'file',
+            'is_active',
+            'created_date',
+            'tags',
+            'reviews',
+            'review_count',
+            'like_count',
+            'liked_by_user',
         ]
+
+    def get_liked_by_user(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            like = obj.likes.filter(user=request.user).first()
+            if like:
+                return like.id
+        return None
+
+    def get_reviews(self, obj):
+        from .serializers import ReviewSerializer  # ReviewSerializer import et
+        return ReviewSerializer(obj.reviews.all(), many=True, context=self.context).data
+
+    def get_tags(self, obj):
+        from .serializers import TagSerializer  # TagSerializer import et
+        return TagSerializer(obj.tags.all(), many=True, context=self.context).data
